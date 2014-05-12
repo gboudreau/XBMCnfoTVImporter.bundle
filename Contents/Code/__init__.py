ccaca2f59c50a55e6b2d7a32c4ae5ee2343855a8
SlrG <michael_wehr@web.de>
2014-05-12 12:32:26 +0200

    fix spelling

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -11,7 +11,7 @@ import os, re, time, datetime, platform, traceback, glob, re, htmlentitydefs
 
 class xbmcnfotv(Agent.TV_Shows):
 	name = 'XBMCnfoTVImporter'
-	version = '1.1-3-g0c86f51-130'
+	version = '1.1-5-g2b00db9-132'
 	primary_provider = True
 	languages = [Locale.Language.NoLanguage]
 	accepts_from = ['com.plexapp.agents.localmedia','com.plexapp.agents.opensubtitles','com.plexapp.agents.podnapisi','com.plexapp.agents.plexthememusic']
