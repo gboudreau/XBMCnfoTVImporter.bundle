1038550b677d97ea061f3f6e9fe6f63c55bc253f
pannal <panni@fragstore.net>
2015-10-19 22:05:59 +0200

    Add Sub-Zero to Agent.accepts_from
    Enables the Sub-Zero agent as requested by https://forums.plex.tv/discussion/comment/1036747/#Comment_1036747

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -18,7 +18,7 @@ class xbmcnfotv(Agent.TV_Shows):
 	ver = '1.1-24-g8b82573-151'
 	primary_provider = True
 	languages = [Locale.Language.NoLanguage]
-	accepts_from = ['com.plexapp.agents.localmedia','com.plexapp.agents.opensubtitles','com.plexapp.agents.podnapisi','com.plexapp.agents.plexthememusic']
+	accepts_from = ['com.plexapp.agents.localmedia','com.plexapp.agents.opensubtitles','com.plexapp.agents.podnapisi','com.plexapp.agents.plexthememusic','com.plexapp.agents.subzero']
 
 ##### helper functions #####
 	def DLog (self, LogMessage):
